fc81bf59e49706175be21c2eea4a7d28d40ea36e
Rayhan-446 <22235103446@cse.bubt.edu.bd>
2025-08-28 11:12:43 +0600

    Step 1: Basic added

--- 01-basics/basics.js
+++ 01-basics/basics.js
@@ -1,11 +1,21 @@
-// Create or switch database
+// Switch (or create) a database
 use myFirstDB;
 
-// Create collection
+// Create a collection named "students"
 db.createCollection("students");
 
-// Insert a document
+// Insert one document
 db.students.insertOne({ name: "Mishu", dept: "CSE", marks: 85 });
 
-// Read documents
+// Insert multiple documents
+db.students.insertMany([
+  { name: "Arif", dept: "EEE", marks: 75 },
+  { name: "Nabila", dept: "CSE", marks: 92 },
+  { name: "Rahim", dept: "BBA", marks: 68 }
+]);
+
+// Read all documents from students collection
 db.students.find();
+
+// Read with pretty format (easier to read)
+db.students.find().pretty();
